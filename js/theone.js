9e0bdf12c2278cf667ceadbdb38b5a7e0daf805d
zijia chen <49104556+nirvanaczj@users.noreply.github.com>
2019-11-27 03:06:12 -0500

    Delete theone.js

--- js/theone.js
+++
@@ -1,5 +0,0 @@
-var theone =  document.getElementById('theone')
-function infoToggle(){
-   theone.innerHTML = "<img src='portasset/InfoGraphics.png'>"
-   TweenMax.to("#theone", 1.5, { ease: Power4.easeOut, y: '100vh' }) 
-}
